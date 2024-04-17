e67f1d1b2bba538825006205ebea7f6009e61c52
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2024-04-17 09:02:04 +0530

    initial commit

---
+++ core-java/src/dataTypes/DataTypes1.java
@@ -0,0 +1,42 @@
+public class DataTypes1 {
+
+	public static void main(String[] args) {
+
+		
+		//non-floating -
+
+		byte variable1 = 127;
+		System.out.println("Byte value is "+ variable1);
+
+		short variable2 = 32767;
+		System.out.println("Short value is "+ variable2);
+
+
+		int variable3 = 2147483647;
+		System.out.println("int value is "+ variable3);
+
+
+		long variable4 = 21474836499L;
+		System.out.println("long value is "+ variable4);
+
+		float variable5 = Float.MAX_VALUE;
+		System.out.println("float value is "+ variable5);
+
+
+		double variable6 = Double.MAX_VALUE;
+		System.out.println("double value is "+ variable6);
+	
+
+		//char variable7 = 97;
+		char variable7 = 'B';
+		System.out.println("char value is "+variable7);
+
+
+		boolean variable8 = false;
+		System.out.println("boolean value is "+variable8);
+
+
+		
+	}
+
+}
