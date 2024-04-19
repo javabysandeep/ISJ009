dd01195fcaab9dfc19789c7433907fb69a594033
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2024-04-19 08:59:08 +0530

    relational, logical operator

---
+++ core-java/src/operators/relational/Demo2.java
@@ -0,0 +1,22 @@
+package operators.relational;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        int number = 10;
+        System.out.println(number % 2 == 0);//true
+        System.out.println(number % 2 != 1);//true
+
+        int year = 2024;
+        System.out.println(year % 400 == 0);//false
+        System.out.println(year % 4 == 0);//true
+        System.out.println(year % 100 == 0);//false
+
+        System.out.println(65=='A');//true
+        System.out.println(65 >'A');//false
+        System.out.println(65 >='A');//true
+        System.out.println(65 <='A');//true
+        System.out.println(65 <'A');//false
+
+
+    }
+}
